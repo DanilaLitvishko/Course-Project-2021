89904faf8eb75cc60406bb89b94988dc618849ab
DanilaLitvishko <litvishko30@mail.ru>
2021-05-14 12:58:16 +0300

    new style

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -3,19 +3,43 @@ var companyApi = Vue.resource('/company{/name}')
 var actApi = Vue.resource('/act{/name}')
 var outputApi = Vue.resource('/output{/id}')
 
-function getIndex(list, id){
-    for(var i = 0; i < list.length; i++){
-        if(list[i].id === id){
-            return i
+function isStringInList(arr, elem){
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].surname === elem) {
+            return true;
         }
     }
-    return -1
+    return false;
+}
+
+function findNameAct(employees, surname){
+    act = ''
+    employees.forEach(employee1 =>{
+        if(employee1.surname == surname){
+            act = employee1.nameOfAction
+        }
+    })
+    return act
+}
+
+function find(acts, surname){
+    console.log()
+    let number = 0;
+    acts.forEach(act => {
+        console.log(act.name + ' ' +  surname)
+        if(act.name == surname){
+            number += act.time
+            console.log('number ' + number + ' time' + act.time)
+        }
+    })
+    return number
 }
 
 Vue.component('employees-form',{
-    props: ['employees', 'socialNetworks', 'acts', 'companys', 'messageAttr'],
+    props: ['employees', 'messageAttr'],
     data: function (){
         return{
+            surname1: '',
             surname: '',
             firstname: '',
             position: '',
@@ -26,32 +50,56 @@ Vue.component('employees-form',{
     },
     watch:{
       messageAttr: function(newVal, oldVal){
-          this.name = newVal.name
-          this.id  = newVal.id
+          this.surname1 = newVal.surname
+          this.surname = newVal.surname
+          this.firstname = newVal.firstname
+          this.position = newVal.position
+          this.nameOfCompany = newVal.nameOfCompany
+          this.nameOfAction = newVal.nameOfAction
+          this.nameOfSocialNetwork = newVal.nameOfSocialNetwork
       }
     },
-   template:'<div>' +
-       '<input type="text" placeholder="Введите фамилию" v-model="surname"/>'+
-       '<input type="text" placeholder="Введите имя" v-model="firstname"/>'+
-       '<input type="text" placeholder="Введите должность" v-model="position"/>'+
-       '<input type="text" placeholder="Введите название компании" v-model="nameOfCompany"/>'+
-       '<input type="text" placeholder="Введите название действия" v-model="nameOfAction"/>'+
-       '<input type="text" placeholder="Введите название социальной сети" v-model="nameOfSocialNetwork"/>'+
-       '<input type="button" value="Сохранить" v-on:click="save"/>' +
+   template:'<div class="container">' +
+       '<div class="row">' +
+            '<div class="col-25">Введите фамилию:</div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите фамилию" v-model="surname"/></div>' +
+       '</div>' +
+       '<div class="row">' +
+            '<div class="col-25">Введите имя:</div>'+
+            '<div class="col-75"><input type="text" placeholder="Введите имя" v-model="firstname"/></div>' +
+       '</div>' +
+       '<div class="row">' +
+            '<div class="col-25">Введите должность:</div>'+
+            '<div class="col-75"><input type="text" placeholder="Введите должность" v-model="position"/></div>' +
+       '</div>' +
+       '<div class="row">'+
+            '<div class="col-25">Введите название компании:</div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите название компании" v-model="nameOfCompany"/></div>' +
+       '</div>' +
+       '<div class="row">'+
+            '<div class="col-25">Введите название действия:</div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите название действия" v-model="nameOfAction"/></div>' +
+       '</div>' +
+       '<div class="row">'+
+            '<div class="col-25">Введите название социальной сети:</div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите название социальной сети" v-model="nameOfSocialNetwork"/></div>' +
+       '</div>'+
+       '<input type="button" value="Сохранить" v-on:click="save" class="button"/>' +
        '</div>',
 
     methods:{
         save: function (){
             var employee = {surname : this.surname, firstname: this.firstname, position: this.position,
                 nameOfSocialNetwork: this.nameOfSocialNetwork, nameOfAction: this.nameOfAction, nameOfCompany: this.nameOfCompany};
-            if(this.surname){
-                employeeApi.update({surname : this.surname, firstname: this.firstname, position: this.position,
+            console.log(isStringInList(this.employees, this.surname1))
+            if(isStringInList(this.surname1, this.surname)){
+                employeeApi.update({surname : this.surname              , firstname: this.firstname, position: this.position,
                     nameOfSocialNetwork: this.nameOfSocialNetwork, nameOfAction: this.nameOfAction, nameOfCompany: this.nameOfCompany}
                     , employee).then(result=>result.json().then(data=> {
-                        var index = getIndex(this.messages, data.id)
-                        this.messages.splice(index, 1, data)
-                        this.name = ''
-                        this.id = ''
+                        //var index = getIndex(this.messages, data.id)
+                        //this.messages.splice(index, 1, data)
+                        //this.name = ''
+                        //this.id = ''
                 }))
             }
             else{
@@ -71,22 +119,21 @@ Vue.component('employees-form',{
 
 Vue.component('employee-row',{
     props: ['employee', 'editMethod', 'employees'],
-    template: '<div>' +
-        'Фамилия: {{employee.surname}},' + ' Имя: {{employee.firstname}},' + ' Должность: {{employee.position}},' + ' Компания: {{employee.nameOfCompany}},'
-        + ' Социальная сеть: {{employee.nameOfSocialNetwork}},' + ' Действие: {{employee.nameOfAction}}' +
-        '<span style="position: absolute; right: 0">' +
-        '<input type="button" value="Изменить" @click="edit"/>'+
-        '<input type="button" value="X" @click="del"/>'+
-        '</span>'+
-        '</div>',
+    template: '<tr>' +
+        '<td>{{employee.surname}}</td><td>{{employee.firstname}}</td><td>{{employee.position}}</td><td>{{employee.nameOfCompany}}</td>' +
+        '<td>{{employee.nameOfSocialNetwork}}</td><td>{{employee.nameOfAction}}</td>' +
+        '<td><input type="button" value="Изменить" @click="edit" class="button"/></td>'+
+        '<td><input type="button" value="X" @click="del" class="button"/></td>'+
+        '</tr>',
     methods:{
         edit: function (){
             this.editMethod(this.employee)
         },
         del: function () {
             employeeApi.remove({surname: this.employee.surname}).then(result=>{
+                console.log(result)
                 if(result.ok){
-                    //this.messages.splice(this.messages.indexOf(this.message), 1)
+
                 }
             })
         }
@@ -100,9 +147,12 @@ Vue.component('employees-list', {
             employee: null
         }
     },
-    template: '<div style="position: relative; width: 1100px"> ' +
-        '<employees-form :employees="employees" :messageAttr="employees" :socialNetworks="socialNetworks" :acts="acts" companys="companys"/>' +
+    template: '<div> ' +
+        '<employees-form :employees="employees" :messageAttr="employee" :socialNetworks="socialNetworks" :acts="acts" companys="companys"/>' +
+        '<table>' +
+        '<tr><td>Фамилия</td><td>Имя</td><td>Должность</td><td>Компания</td><td>Социальная сеть</td><td>Действие</td><td>Изменить</td><td>Удалить</td></tr>' +
         '<employee-row v-for="employee in employees" :employee="employee" :editMethod="editMethod" :employees="employees"/>' +
+        '</table>' +
         '</div>',
     methods:{
         editMethod: function (employee){
@@ -119,10 +169,17 @@ Vue.component('companys-form', {
             address: ''
         }
     },
-    template: '<div>' +
-        '<input type="text" placeholder="Введите название компании" v-model="name"/>'+
-        '<input type="text" placeholder="Введите адрес компании" v-model="address"/>'+
-        '<input type="button" value="Сохранить" v-on:click="save"/>' +
+    template:
+        '<div class="container">' +
+            '<div class="row">' +
+                '<div class="col-25">Введите название компании: </div>' +
+                '<div class="col-75"><input type="text" placeholder="Введите название компании" v-model="name"/></div>' +
+            '</div>'+
+            '<div class="row">' +
+                '<div class="col-25">Введите название компании: </div>' +
+                '<div class="col-75"><input type="text" placeholder="Введите адрес компании" v-model="address"/></div>'+
+            '</div>' +
+            '<input type="button" value="Сохранить" v-on:click="save" class="button"/>'+
         '</div>',
 
     methods: {
@@ -148,13 +205,11 @@ Vue.component('companys-form', {
 
 Vue.component('company-row',{
     props: ['company', 'editMethod', 'companys'],
-    template: '<div>' +
-        '{{company.name}} ' + '{{company.address}} ' +
-        '<span style="position: absolute; right: 0">' +
-        '<input type="button" value="Изменить" @click="edit"/>'+
-        '<input type="button" value="X" @click="del"/>'+
-        '</span>'+
-        '</div>',
+    template: '<tr>' +
+        '<td>{{company.name}}</td> ' + '<td>{{company.address}}</td>' +
+        '<td><input type="button" value="Изменить" @click="edit" class="button"/></td>'+
+        '<td><input type="button" value="X" @click="del" class="button"/></td>'+
+        '</tr>',
     methods:{
         edit: function (){
             this.editMethod(this.employee)
@@ -171,9 +226,12 @@ Vue.component('company-row',{
 
 Vue.component('companys-list',{
     props:['companys'],
-    template: '<div style="position: relative; width: 1100px"> ' +
+    template: '<div> ' +
         '<companys-form :companys="companys"/>' +
+        '<table>' +
+        '<tr><td>Название компании</td><td>Адрес</td><td>Изменить</td><td>Удалить</td></tr>' +
         '<company-row v-for="company in companys" :company="company" :companys="companys"/>' +
+        '</table>' +
         '</div>',
 
 });
@@ -187,11 +245,17 @@ Vue.component('acts-form', {
             time: ''
         }
     },
-    template: '<div>' +
-        '<input type="text" placeholder="Введите название действия" v-model="name"/>'+
+    template: '<div class="container">' +
+        '<div class="row">' +
+            '<div class="col-25">Введите название действия: </div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите название действия" v-model="name"/></div>' +
+        '</div>'+
         //'<input type="text" placeholder="Введите описание действия" v-model="core"/>'+
-        '<input type="text" placeholder="Введите время(в секундах) на выполнение" v-model="time"/>'+
-        '<input type="button" value="Сохранить" v-on:click="save"/>' +
+        '<div class="row">' +
+            '<div class="col-25">Введите время(в секундах): </div>' +
+            '<div class="col-75"><input type="text" placeholder="Введите время(в секундах) на выполнение" v-model="time"/></div>' +
+        '</div>'+
+        '<input type="button" value="Сохранить" v-on:click="save" class="button"/>' +
         '</div>',
 
     methods: {
@@ -218,13 +282,11 @@ Vue.component('acts-form', {
 
 Vue.component('act-row',{
     props: ['act', 'editMethod', 'acts'],
-    template: '<div>' +
-        '{{act.name}} ' + '{{act.core}} ' + '{{act.time}} ' +
-        '<span style="position: absolute; right: 0">' +
-        '<input type="button" value="Изменить" @click="edit"/>'+
-        '<input type="button" value="X" @click="del"/>'+
-        '</span>'+
-        '</div>',
+    template: '<tr>' +
+        '<td>{{act.name}}</td> ' + '<td>{{act.time}}</td> ' +
+        '<td><input type="button" value="Изменить" @click="edit" class="button"/></td>'+
+        '<td><input type="button" value="X" @click="del" class="button"/></td>'+
+        '</tr>',
     methods:{
         edit: function (){
             this.editMethod(this.employee)
@@ -241,9 +303,12 @@ Vue.component('act-row',{
 
 Vue.component('acts-list',{
     props:['acts'],
-    template: '<div style="position: relative; width: 1100px"> ' +
+    template: '<div> ' +
         '<acts-form :acts="acts"/>' +
+        '<table>' +
+        '<tr><td>Название действия</td><td>Время на выполнение(в секундах)</td><td>Изменить</td><td>Удалить</td></tr>' +
         '<act-row v-for="act in acts" :act="act" :acts="acts"/>' +
+        '</table>' +
         '</div>',
 
 });
@@ -260,32 +325,61 @@ Vue.component('persent-calculator', {
         ' % от числа ' +
         '<input type="text" placeholder="Введите число" v-model="number"/>' +
         ' = {{result}} ' +
-        '<input type="button" value="Расчитать" @click="result=(percent * number)/100"/>' +
+        '<input type="button" value="Рассчитать" @click="result=(percent * number)/100" class="button"/>' +
         '</div>'
 })
 
 Vue.component('adversiting-article',{
-    template: '<div>Написание рекламной статьи:<p><textarea cols="60" rows="8"></textarea></p></div>'
+    data: function (){
+        return {
+            text: '',
+            article: ''
+        }},
+    template: '<div>' +
+            'Написание рекламной статьи:<p><textarea cols="60" rows="8" v-model="article"></textarea></p>' +
+            '<input type="button" value="Просмотреть" @click="text = article" class="button"/>' +
+        '<div>{{text}}</div>' +
+        '</div>',
+})
+
+Vue.component('text-info', {
+    props:['info'],
+    template: '<div>{{info}}</div>'
 })
 
 Vue.component('analyzer', {
+    props: ['employees', 'acts'],
     data:{
         function(){
             return{
-                surname: ''
+                inputValue:'',
+                surname: '',
+                output: 'HUI'
             }
         }
     },
     template: '<p>' +
         '<input type="text" placeholder="Введите фамилию анализируемого сотрудника" v-model="surname"/>' +
-        '<input type="button" value="Проанализировать" @click="analyze"/>' +
+        '<input type="button" value="Проанализировать" @click="print" class="button"/>' +
+        '<text-info :info="output"/>' +
         '</p>',
-    methods: {
-        analyze: function (){
-
+    methods:
+        {
+            print: function () {
+                act = findNameAct(this.employees, this.surname)
+                number = find(this.acts, act)
+                console.log(number)
+                this.output = 'sssss'
+                if (number < 1000) {
+                    this.output = 'Социальные сети сотрудника не мешают его работе'
+                } else {
+                    this.output = 'Социальные сети сотрудника мешают его работе'
+                }
+                console.log(this.output)
+            }
         }
     }
-})
+)
 
 var app = new Vue({
     el: '#app',
@@ -297,7 +391,7 @@ var app = new Vue({
                 '<companys-list :companys = "companys"/>' +
                 '<acts-list :acts = "acts"/>' +
                 '<persent-calculator/>' +
-                '<analyzer/>' +
+                //'<analyzer :employees = "employees" :acts="acts"/>' +
                 '<adversiting-article/>' +
             '</div>' +
         '</div>',
@@ -309,10 +403,5 @@ var app = new Vue({
         employees: frontendData.employees,
         outputs: frontendData.outputs,
         socialNetworks: frontendData.socialNetworks
-    },
-    created: function (){
-//        messageApi.get().then(result=>result.json().then(
-//            data=>data.forEach(message => this.messages.push(message))
-//        ))
     },
 });
